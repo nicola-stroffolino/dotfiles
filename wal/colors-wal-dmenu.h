5515444f8eae7fb26d78762f6d12cc9352a09278
Nicola Stroffolino <nicola.stroffolino05@gmail.com>
2024-09-07 18:04:05 +0200

    wal and some fixes

---
+++ wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ebddbc", "#background" },
+	[SchemeSel] = { "#ebddbc", "#967786" },
+	[SchemeOut] = { "#ebddbc", "#DEC386" },
+};
