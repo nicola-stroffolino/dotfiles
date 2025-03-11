ec5db0c6152d681123d9aad58eb66b379cb96e62
Nicola Stroffolino <nicola.stroffolino05@gmail.com>
2025-03-11 10:31:01 +0100

    cherry picking the scripts i need

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ebddbc", "#background" },
-	[SchemeSel] = { "#ebddbc", "#967786" },
-	[SchemeOut] = { "#ebddbc", "#DEC386" },
-};
